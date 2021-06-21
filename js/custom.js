95ece2e3bf62dfcae3e28a43170dcbd9e1b068d1
Adel Abu Ghazaleh <adel.m.abughazaleh@gmail.com>
2021-06-22 01:24:44 +0300

    Nice

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#contact-form").submit(function(e){
-        console.log('We entered the submitted form 01:22AM')
+        console.log('We entered the submitted form 01:26AM')
         e.preventDefault()
         var data = {
             "mail":'aldoaa'
