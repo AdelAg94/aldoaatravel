3e52b2a495baf2a70b50ca0a0e6692b8d7a63c98
Adel Abu Ghazaleh <adel.m.abughazaleh@gmail.com>
2021-06-22 19:22:13 +0300

    Send Mail correctly

--- js/custom.js
+++ js/custom.js
@@ -2,28 +2,38 @@ $(document).ready(function(){
     $("#contact-form").submit(function(e){
         console.log('We entered the submitted form 01:26AM')
         e.preventDefault()
+        var f_name = $("#name").val()
+        var f_email = $("#email").val()
+        var f_subject = $("#subject").val()
+        var f_message = $("#message").val()
         var data = {
-            "mail":'aldoaa'
+            "mail":'aldoaa',
+            'name':f_name,
+            "sender":f_email,
+            'subject':f_subject,
+            'message':f_message,
         }
         var mailurl = 'https://ovq9z0uac8.execute-api.us-west-2.amazonaws.com/dev/sendMail' 
         // var mailurl_2 = ' https://yymk086tm1.execute-api.us-west-2.amazonaws.com/prod/rsendmail' 
         $.ajax({
             url: mailurl,
-            // dataType: "json",
-            // contentType: "application/json",
-            // data: JSON.stringify(data),
+            dataType: "json",
+            data: data,
             type: 'GET',
             
             success: function(result) {
                  // success code execution here
                  console.log(result)
-                 alert(result)
-        
+                 alert(result['message'])
+                 $("#name").val('')
+                 $("#email").val('')
+                 $("#subject").val('')
+                 $("#message").val('')
             },
             error: function(xhr,status,error) {
                // error code here
                console.log(error)
-               alert(error)
+               alert('عذرا! حدث خطأ يرجى محاولة الإرسال مرة أخرى')
             },
             complete: function(xhr,status) {
                  // completion code here
